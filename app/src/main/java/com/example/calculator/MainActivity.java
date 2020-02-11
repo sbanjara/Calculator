09357af1a6b8e755635e75ec0b5f0637d0642f01
Sabin Banjara <sbanjara@stu.jsu.edu>
2020-02-11 15:20:32 -0600

    Creation of Switch statement to handle button click operation

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -16,8 +16,12 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
+	private String s;
+	private int index;
+	private BigDecimal lhs;
+	private BigDecimal rhs;
+	private TextView output;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -34,17 +38,19 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-
-        TextView output = (TextView) findViewById(R.id.outputView);
+		
+		s = "";
+		index = 0;
+		lhs = new BigDecimal("0");
+		rhs = new BigDecimal("0");
+        output = (TextView) findViewById(R.id.outputView);
         output.setText(String.valueOf(0));
 
     }
 
     public void buttonClick(View v) {
 
-        String s = "";
         String buttonText = ((Button) v).getText().toString();
-        TextView output = (TextView) findViewById(R.id.outputView);
 
         switch(buttonText) {
 
@@ -62,11 +68,86 @@ public class MainActivity extends AppCompatActivity {
 
             case "√":
                 s += "√";
+				index = s.indexOf("√");
+				lhs = new BigDecimal(s.substring(0, index));
+                break;
+				
+			case "4":
+                s += "4";
+                break;
+
+            case "5":
+                s += "5";
+                break;
+
+            case "6":
+                s += "6";
+                break;
+
+            case "\u00F7":
+                s += "\u00F7";
+				index = s.indexOf("\u00F7");
+				lhs = new BigDecimal(s.substring(0, index));
+                break;
+				
+			case "%":
+				s = "%";
+				index = s.indexOf("%");
+				lhs = new BigDecimal(s.substring(0, index));
+				break;
+				
+			case "1":
+                s += "1";
+                break;
+
+            case "2":
+                s += "2";
+                break;
+
+            case "3":
+                s += "3";
+                break;
+
+            case "\u00D7":
+                s += "\u00D7";
+				index = s.indexOf("\u00D7");
+				lhs = new BigDecimal(s.substring(0, index));
+                break;
+				
+			case "-":
+				s = "-";
+				index = s.indexOf("-");
+				lhs = new BigDecimal(s.substring(0, index));
+				break;
+				
+			case "\u00B1":
+                s += "\u00B1";
+                break;
+				
+			case "0":
+				s = "0";
+				break;
+
+            case ".":
+                s += ".";
+                break;
+
+            case "+":
+                s += "+";
+				index = s.indexOf("+");
+				lhs = new BigDecimal(s.substring(0, index));
+                break;
+
+            case "=":
+                s += "=";
+				index = s.indexOf("+");
+				lhs = new BigDecimal(s.substring(0, index));
                 break;
 
             default:
-                s += "0";
+                s = "0";
                 break;
+				
         }
 
         output.setText(s);
